7242335dbf97af580fb08c2465013d32751d06db
RicardoMarin9303 <rlmz0327@gmail.com>
2023-07-30 10:07:37 +0200

    Hola a todos

---
+++ consultar_ip.py
@@ -0,0 +1,16 @@
+import requests
+
+def obtener_ip_publica():
+    respuesta = requests.get('https://api.ipify.org')
+    if respuesta.status_code == 200:
+        ip_publica = respuesta.text
+        return ip_publica
+    else:
+        return None
+
+ip = obtener_ip_publica()
+
+if ip:
+    print("La IP pública de tu equipo es:", ip)
+else:
+    print("No se pudo obtener la IP pública de tu equipo")
